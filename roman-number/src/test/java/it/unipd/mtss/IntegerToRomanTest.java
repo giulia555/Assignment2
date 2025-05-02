4d05031248e3fb42395a6d286f7bbde7682cd7c1
Lisa-csg <lisa.casagrande.3@studenti.unipd.it>
2025-05-02 14:04:57 +0200

    implementato test3
    close #6

--- roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -2,3 +2,19 @@
 // [Giulia] [Barzon] [2101074] 
 // [Lisa] [Casagrande] [2116440] 
 ////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class IntegerToRomanTest {
+    @Test
+    public void testConvert() {
+        assertEquals("I", IntegerToRoman.convert(1));
+        assertEquals("II", IntegerToRoman.convert(2));
+        assertEquals("III", IntegerToRoman.convert(3));
+
+    }
+}
--- roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -2,3 +2,18 @@
 // [Giulia] [Barzon] [2101074] 
 // [Lisa] [Casagrande] [2116440] 
 ////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class RomanPrinterTest {
+    @Test
+    public void testPrint() {
+        assertEquals("  _____ \n |_   _|\n   | |  \n   | |  \n  _| |_ \n |_____|\n", RomanPrinter.print(1));
+        assertEquals("  _____   _____ \n |_   _| |_   _|\n   | |     | |  \n   | |     | |  \n  _| |_   _| |_ \n |_____| |_____|\n", RomanPrinter.print(2));
+        assertEquals("  _____   _____   _____ \n |_   _| |_   _| |_   _|\n   | |     | |     | |  \n   | |     | |     | |  \n  _| |_   _| |_   _| |_ \n |_____| |_____| |_____|\n", RomanPrinter.print(3));
+    }
+}
