c2a449619619170efbaa8b626250e5fa6d6a5967
[giulia555] <[giuliabarz04@gmail.com]>
2025-05-02 15:14:02 +0200

    Implementato test 6 close #7

--- roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -12,9 +12,17 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class IntegerToRomanTest {
     @Test
     public void testConvert() {
+        //test 3
         assertEquals("I", IntegerToRoman.convert(1));
         assertEquals("II", IntegerToRoman.convert(2));
         assertEquals("III", IntegerToRoman.convert(3));
 
+        //test 6
+        assertEquals("IV", IntegerToRoman.convert(4));
+        assertEquals("V", IntegerToRoman.convert(5));
+        assertEquals("VI", IntegerToRoman.convert(6));
+
+        //test 10
+        
     }
 }
